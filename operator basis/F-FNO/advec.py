16b1f859032acac323eedf8341ff750b6ec899e3
YuYu8900 <1925350557@qq.com>
2025-05-03 23:14:55 +0800

    basis

---
+++ operator basis/F-FNO/advec.py
@@ -0,0 +1,287 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES']='0'
+from matplotlib.pyplot import flag
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
+from utilities3 import *
+from linear import WNLinear
+from feedforward import FeedForward
+from timeit import default_timer
+
+
+
+class SpectralConv1d(nn.Module):
+    def __init__(self, in_dim, out_dim, modes, forecast_ff, backcast_ff,
+                 fourier_weight, factor, ff_weight_norm,
+                 n_ff_layers, layer_norm, use_fork, dropout, mode):
+        super().__init__()
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.modes = modes
+        self.mode = mode
+        self.use_fork = use_fork
+
+        self.fourier_weight = fourier_weight
+        # Can't use complex type yet. See https://github.com/pytorch/pytorch/issues/59998
+        if not self.fourier_weight:
+            self.fourier_weight = nn.ParameterList([])
+            for n_modes in [modes]:
+                weight = torch.FloatTensor(in_dim, out_dim, n_modes, 2)
+                param = nn.Parameter(weight)
+                nn.init.xavier_normal_(param)
+                self.fourier_weight.append(param)
+
+        if use_fork:
+            self.forecast_ff = forecast_ff
+            if not self.forecast_ff:
+                self.forecast_ff = FeedForward(
+                    out_dim, factor, ff_weight_norm, n_ff_layers, layer_norm, dropout)
+
+        self.backcast_ff = backcast_ff
+        if not self.backcast_ff:
+            self.backcast_ff = FeedForward(
+                out_dim, factor, ff_weight_norm, n_ff_layers, layer_norm, dropout)
+
+    def forward(self, x):
+        # x.shape == [batch_size, grid_size, grid_size, in_dim]
+        if self.mode != 'no-fourier':
+            x = self.forward_fourier(x)
+
+        b = self.backcast_ff(x)
+        f = self.forecast_ff(x) if self.use_fork else None
+        return b, f
+
+    def forward_fourier(self, x):
+        x = rearrange(x, 'b m i -> b i m')
+        # x.shape == [batch_size, in_dim, grid_size, grid_size]
+
+        B, I, M = x.shape
+
+        # # # Dimesion X # # #
+        x_ftx = torch.fft.rfft(x, dim=-1, norm='ortho')
+        # x_ft.shape == [batch_size, in_dim, grid_size, grid_size // 2 + 1]
+
+        out_ft = x_ftx.new_zeros(B, I, M// 2 + 1)
+        # out_ft.shape == [batch_size, in_dim, grid_size, grid_size // 2 + 1, 2]
+
+        if self.mode == 'full':
+            out_ft[:, :,  :self.modes] = torch.einsum(
+                "bix,iox->box",
+                x_ftx[:, :,  :self.modes],
+                torch.view_as_complex(self.fourier_weight[0]))
+        elif self.mode == 'low-pass':
+            out_ft[:, :, :self.modes] = x_ftx[:, :, :, :self.modes]
+
+        x = torch.fft.irfft(out_ft, n=M, dim=-1, norm='ortho')
+        # x.shape == [batch_size, in_dim, grid_size, grid_size]
+
+        x = rearrange(x, 'b i m -> b m i')
+        # x.shape == [batch_size, grid_size, grid_size, out_dim]
+
+        return x
+
+
+class FNOFactorizedMesh1D(nn.Module):
+    def __init__(self, modes, width, input_dim, n_layers, share_weight, factor,
+                 ff_weight_norm, n_ff_layers, layer_norm):
+        super().__init__()
+        self.padding = 2  # pad the domain if input is non-periodic
+        self.modes = modes
+        self.width = width
+        self.input_dim = input_dim
+        self.in_proj = WNLinear(input_dim, self.width, wnorm=ff_weight_norm)
+        self.n_layers = n_layers
+
+        self.fourier_weight = None
+        if share_weight:
+            self.fourier_weight = nn.ParameterList([])
+            for n_modes in [modes]:
+                weight = torch.FloatTensor(width, width, n_modes, 2)
+                param = nn.Parameter(weight)
+                nn.init.xavier_normal_(param)
+                self.fourier_weight.append(param)
+
+        self.spectral_layers = nn.ModuleList([])
+        for _ in range(n_layers):
+            self.spectral_layers.append(SpectralConv1d(in_dim=width,
+                                                       out_dim=width,
+                                                       modes=modes,
+                                                       forecast_ff=None,
+                                                       backcast_ff=None,
+                                                       fourier_weight=self.fourier_weight,
+                                                       factor=factor,
+                                                       ff_weight_norm=ff_weight_norm,
+                                                       n_ff_layers=n_ff_layers,
+                                                       layer_norm=layer_norm,
+                                                       use_fork=False,
+                                                       dropout=0.0,
+                                                       mode='full'))
+
+        self.out = nn.Sequential(
+            WNLinear(self.width, 128, wnorm=ff_weight_norm),
+            WNLinear(128, 1, wnorm=ff_weight_norm))
+
+    def forward(self, x):
+        # grid = self.get_grid(x.shape, x.device)
+        # x = torch.cat((x, grid), dim=-1)  # [B, X, Y, 4]
+        x = self.in_proj(x)  # [B, X, H]
+        x = x.permute(0, 2, 1)  # [B, H, X]
+        # x = F.pad(x, [0, self.padding])
+        x = x.permute(0, 2, 1)  # [B, X, H]
+
+        for i in range(self.n_layers):
+            layer = self.spectral_layers[i]
+            b, _ = layer(x)
+            x = x + b
+
+        # b = b[..., :-self.padding]
+        output = self.out(b)
+
+        return output
+
+    def get_grid(self, shape, device):
+        batchsize, size_x = shape[0], shape[1]
+        gridx = torch.tensor(np.linspace(0, 1, size_x), dtype=torch.float)
+        gridx = gridx.reshape(1, size_x, 1).repeat([batchsize, 1, 1])
+        return gridx.to(device)
+
+ntrain = 1000
+ntest = 100
+s = 40
+
+batch_size = 100
+learning_rate = 0.001
+
+epochs = 500
+scheduler_step = 50
+scheduler_gamma = 0.5
+
+T = 40
+step = 1
+
+modes = 16
+width = 32
+input_dim = 2
+n_layers = 4#4
+share_weight = 'false'
+factor = 4  
+ff_weight_norm ='true'
+n_ff_layers = 1
+layer_norm = 'false'
+
+################################################################
+# read data
+################################################################
+
+# Data is of the shape (number of samples, grid size)
+data = np.load('/home/yy/yy/data/time_advection/train_IC2.npz')
+x, t, u_train = data["x"], data["t"], data["u"]  # N x nt x nx
+u0_train = u_train[:ntrain, 0, :]  # N x nx
+x = np.repeat(x[0:1, :], ntrain, axis=0)  # N x nx
+x_train = np.concatenate((u0_train[:, :, None], x[:, :, None]), axis=-1)  # N x nx x 2
+u_train = u_train.transpose(0, 2, 1)[:ntrain,:,:]  # N x nx x nt
+x_train = torch.from_numpy(x_train)
+u_train = torch.from_numpy(u_train)
+train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_train, u_train), batch_size=batch_size, shuffle=True)
+
+data = np.load('/home/yy/yy/data/time_advection/test_IC2.npz')
+x, t, u_test = data["x"], data["t"], data["u"]  # N x nt x nx
+u0_test = u_test[:ntest, 0, :]  # N x nx
+x = np.repeat(x[0:1, :], ntest, axis=0)  # N x nx
+x_test = np.concatenate((u0_test[:, :, None], x[:, :, None]), axis=-1)  # N x nx x 2
+u_test = u_test.transpose(0, 2, 1)  # N x nx x nt
+x_test = torch.from_numpy(x_test)
+u_test = torch.from_numpy(u_test[:ntest,:,:])
+test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_test, u_test), batch_size=batch_size, shuffle=False)
+
+
+# train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_train, y_train),
+#                                            batch_size=batch_size, shuffle=True)
+# test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_test, y_test),
+#                                           batch_size=batch_size, shuffle=False)
+
+# %%
+""" The model definition """
+model = FNOFactorizedMesh1D(modes, width, input_dim, n_layers, share_weight, factor,
+                 ff_weight_norm, n_ff_layers, layer_norm).to(device)
+print(count_params(model))
+
+
+""" Training and testing """
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
+
+start_time = default_timer()
+train_loss = torch.zeros(epochs)
+test_loss = torch.zeros(epochs)
+myloss = LpLoss(size_average=False)
+for ep in range(epochs):
+    t1 = default_timer()
+    model.train()
+    train_l2_step = 0
+    train_l2_full = 0
+    for xx, yy in train_loader:
+        loss = 0
+        xx = xx.to(device)
+        yy = yy.to(device)
+
+        for t in range(0, T, step):
+            y = yy[..., t:t + step] # t:t+step, retains the third dimension,
+
+            im = model(xx)            
+            loss += myloss(im.reshape(batch_size, -1), y.reshape(batch_size, -1))
+            
+            if t == 0:
+                pred = im
+            else:
+                pred = torch.cat((pred, im), -1)
+
+            xx = torch.cat((xx[..., step:], im), dim=-1)
+
+        # pred = yy[:, :, :1]  # t = 0
+        # for t in range(1, T, 1):
+        #     im = model(xx)
+        #     pred = torch.cat((pred, im), -1)
+        #     xx = torch.cat((im, xx[:, :, 1:]), dim=-1)  # cat x
+
+        train_l2_step += loss.item()
+        l2_full = myloss(pred.reshape(batch_size, -1), yy.reshape(batch_size, -1))
+        train_l2_full += l2_full.item()
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    test_l2_step = 0
+    test_l2_full = 0
+    with torch.no_grad():
+        for xx, yy in test_loader:
+            loss = 0
+            xx = xx.to(device)
+            yy = yy.to(device)
+
+            for t in range(0, T, step):
+                y = yy[..., t:t + step]
+                im = model(xx)
+                loss += myloss(im.reshape(batch_size, -1), y.reshape(batch_size, -1))
+
+                if t == 0:
+                    pred = im
+                else:
+                    pred = torch.cat((pred, im), -1)
+
+                xx = torch.cat((xx[..., step:], im), dim=-1)
+
+            test_l2_step += loss.item()
+            test_l2_full += myloss(pred.reshape(batch_size, -1), yy.reshape(batch_size, -1)).item()
+
+    train_loss[ep] = train_l2_step/ntrain/(T/step)
+    test_loss[ep] = test_l2_step/ntest/(T/step)
+    
+    t2 = default_timer()
+    scheduler.step()
+    print(ep, t2 - t1, train_l2_step / ntrain / (T / step), train_l2_full / ntrain, test_l2_step / ntest / (T / step),
+          test_l2_full / ntest)
---
+++ operator basis/F-FNO/darcy.py
@@ -0,0 +1,281 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES']='1'
+from matplotlib.pyplot import flag
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
+from utilities3 import *
+from linear import WNLinear
+from feedforward import FeedForward
+from timeit import default_timer
+
+
+
+class SpectralConv2d(nn.Module):
+    def __init__(self, in_dim, out_dim, modes_x, modes_y, forecast_ff, backcast_ff,
+                 fourier_weight, factor, ff_weight_norm,
+                 n_ff_layers, layer_norm, use_fork, dropout, mode):
+        super().__init__()
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.modes_x = modes_x
+        self.modes_y = modes_y
+        self.mode = mode
+        self.use_fork = use_fork
+
+        self.fourier_weight = fourier_weight
+        # Can't use complex type yet. See https://github.com/pytorch/pytorch/issues/59998
+        if not self.fourier_weight:
+            self.fourier_weight = nn.ParameterList([])
+            for n_modes in [modes_x, modes_y]:
+                weight = torch.FloatTensor(in_dim, out_dim, n_modes, 2)
+                param = nn.Parameter(weight)
+                nn.init.xavier_normal_(param)
+                self.fourier_weight.append(param)
+
+        if use_fork:
+            self.forecast_ff = forecast_ff
+            if not self.forecast_ff:
+                self.forecast_ff = FeedForward(
+                    out_dim, factor, ff_weight_norm, n_ff_layers, layer_norm, dropout)
+
+        self.backcast_ff = backcast_ff
+        if not self.backcast_ff:
+            self.backcast_ff = FeedForward(
+                out_dim, factor, ff_weight_norm, n_ff_layers, layer_norm, dropout)
+
+    def forward(self, x):
+        # x.shape == [batch_size, grid_size, grid_size, in_dim]
+        if self.mode != 'no-fourier':
+            x = self.forward_fourier(x)
+
+        b = self.backcast_ff(x)
+        f = self.forecast_ff(x) if self.use_fork else None
+        return b, f
+
+    def forward_fourier(self, x):
+        x = rearrange(x, 'b m n i -> b i m n')
+        # x.shape == [batch_size, in_dim, grid_size, grid_size]
+
+        B, I, M, N = x.shape
+
+        # # # Dimesion Y # # #
+        x_fty = torch.fft.rfft(x, dim=-1, norm='ortho')
+        # x_ft.shape == [batch_size, in_dim, grid_size, grid_size // 2 + 1]
+
+        out_ft = x_fty.new_zeros(B, I, M, N // 2 + 1)
+        # out_ft.shape == [batch_size, in_dim, grid_size, grid_size // 2 + 1, 2]
+
+        if self.mode == 'full':
+            out_ft[:, :, :, :self.modes_y] = torch.einsum(
+                "bixy,ioy->boxy",
+                x_fty[:, :, :, :self.modes_y],
+                torch.view_as_complex(self.fourier_weight[1]))
+        elif self.mode == 'low-pass':
+            out_ft[:, :, :, :self.modes_y] = x_fty[:, :, :, :self.modes_y]
+
+        xy = torch.fft.irfft(out_ft, n=N, dim=-1, norm='ortho')
+        # x.shape == [batch_size, in_dim, grid_size, grid_size]
+
+        # # # Dimesion X # # #
+        x_ftx = torch.fft.rfft(x, dim=-2, norm='ortho')
+        # x_ft.shape == [batch_size, in_dim, grid_size // 2 + 1, grid_size]
+
+        out_ft = x_ftx.new_zeros(B, I, M // 2 + 1, N)
+        # out_ft.shape == [batch_size, in_dim, grid_size // 2 + 1, grid_size, 2]
+
+        if self.mode == 'full':
+            out_ft[:, :, :self.modes_x, :] = torch.einsum(
+                "bixy,iox->boxy",
+                x_ftx[:, :, :self.modes_x, :],
+                torch.view_as_complex(self.fourier_weight[0]))
+        elif self.mode == 'low-pass':
+            out_ft[:, :, :self.modes_x, :] = x_ftx[:, :, :self.modes_x, :]
+
+        xx = torch.fft.irfft(out_ft, n=M, dim=-2, norm='ortho')
+        # x.shape == [batch_size, in_dim, grid_size, grid_size]
+
+        # # Combining Dimensions # #
+        x = xx + xy
+
+        x = rearrange(x, 'b i m n -> b m n i')
+        # x.shape == [batch_size, grid_size, grid_size, out_dim]
+
+        return x
+
+
+class FNOFactorizedMesh2D(nn.Module):
+    def __init__(self, modes_x, modes_y, width, input_dim, n_layers, share_weight, factor,
+                 ff_weight_norm, n_ff_layers, layer_norm):
+        super().__init__()
+        self.padding = 8  # pad the domain if input is non-periodic
+        self.modes_x = modes_x
+        self.modes_y = modes_y
+        self.width = width
+        self.input_dim = input_dim
+        self.in_proj = WNLinear(input_dim, self.width, wnorm=ff_weight_norm)
+        self.n_layers = n_layers
+
+        self.fourier_weight = None
+        if share_weight:
+            self.fourier_weight = nn.ParameterList([])
+            for n_modes in [modes_x, modes_y]:
+                weight = torch.FloatTensor(width, width, n_modes, 2)
+                param = nn.Parameter(weight)
+                nn.init.xavier_normal_(param)
+                self.fourier_weight.append(param)
+
+        self.spectral_layers = nn.ModuleList([])
+        for _ in range(n_layers):
+            self.spectral_layers.append(SpectralConv2d(in_dim=width,
+                                                       out_dim=width,
+                                                       modes_x=modes_x,
+                                                       modes_y=modes_y,
+                                                       forecast_ff=None,
+                                                       backcast_ff=None,
+                                                       fourier_weight=self.fourier_weight,
+                                                       factor=factor,
+                                                       ff_weight_norm=ff_weight_norm,
+                                                       n_ff_layers=n_ff_layers,
+                                                       layer_norm=layer_norm,
+                                                       use_fork=False,
+                                                       dropout=0.0,
+                                                       mode='full'))
+
+        self.out = nn.Sequential(
+            WNLinear(self.width, 128, wnorm=ff_weight_norm),
+            WNLinear(128, 1, wnorm=ff_weight_norm))
+
+    def forward(self, x):
+        grid = self.get_grid(x.shape, x.device)
+        x = torch.cat((x, grid), dim=-1)  # [B, X, Y, 4]
+        x = self.in_proj(x)  # [B, X, Y, H]
+        x = x.permute(0, 3, 1, 2)  # [B, H, X, Y]
+        x = F.pad(x, [0, self.padding, 0, self.padding])
+        x = x.permute(0, 2, 3, 1)  # [B, X, Y, H]
+
+        for i in range(self.n_layers):
+            layer = self.spectral_layers[i]
+            b, _ = layer(x)
+            x = x + b
+
+        b = b[..., :-self.padding, :-self.padding, :]
+        output = self.out(b)
+
+        return output
+
+    def get_grid(self, shape, device):
+        batchsize, size_x, size_y = shape[0], shape[1], shape[2]
+        gridx = torch.tensor(np.linspace(0, 1, size_x), dtype=torch.float)
+        gridx = gridx.reshape(1, size_x, 1, 1).repeat(
+            [batchsize, 1, size_y, 1])
+        gridy = torch.tensor(np.linspace(0, 1, size_y), dtype=torch.float)
+        gridy = gridy.reshape(1, 1, size_y, 1).repeat(
+            [batchsize, size_x, 1, 1])
+        return torch.cat((gridx, gridy), dim=-1).to(device)
+
+
+
+TRAIN_PATH = '/home/yy/yy/data/Darcy_421/piececonst_r421_N1024_smooth1.mat'
+TEST_PATH = '/home/yy/yy/data/Darcy_421/piececonst_r421_N1024_smooth2.mat'
+ntrain = 1000
+ntest = 100
+
+batch_size = 20
+learning_rate = 0.001
+
+epochs = 500
+step_size = 50
+gamma = 0.75
+
+
+modes = 16
+width = 32
+input_dim = 3
+n_layers = 24
+share_weight = 'false'
+factor = 4
+ff_weight_norm ='true'
+n_ff_layers = 2
+layer_norm = 'false'
+
+
+r = 5
+h = int(((421 - 1)/r) + 1)
+s = h
+
+# %%
+""" Read data """
+reader = MatReader(TRAIN_PATH)
+x_train = reader.read_field('coeff')[:ntrain,::r,::r][:,:s,:s]
+y_train = reader.read_field('sol')[:ntrain,::r,::r][:,:s,:s]
+
+reader.load_file(TEST_PATH)
+x_test = reader.read_field('coeff')[:ntest,::r,::r][:,:s,:s]
+y_test = reader.read_field('sol')[:ntest,::r,::r][:,:s,:s]
+
+x_normalizer = UnitGaussianNormalizer(x_train)
+x_train = x_normalizer.encode(x_train)
+x_test = x_normalizer.encode(x_test)
+
+y_normalizer = UnitGaussianNormalizer(y_train)
+y_train = y_normalizer.encode(y_train)
+
+x_train = x_train.reshape(ntrain,s,s,1)
+x_test = x_test.reshape(ntest,s,s,1)
+
+train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_train, y_train), batch_size=batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_test, y_test), batch_size=batch_size, shuffle=False)
+
+""" The model definition """
+model = FNOFactorizedMesh2D(modes, modes, width, input_dim, n_layers, share_weight, factor,
+                 ff_weight_norm, n_ff_layers, layer_norm).to(device)
+print(count_params(model))
+
+""" Training and testing """
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
+
+train_loss = torch.zeros(epochs)
+test_loss = torch.zeros(epochs)
+myloss = LpLoss(size_average=False)
+y_normalizer.cuda()
+for ep in range(epochs):
+    model.train()
+    t1 = default_timer()
+    train_l2 = 0
+    for x, y in train_loader:
+        x, y = x.to(device), y.to(device)
+
+        optimizer.zero_grad()
+        out = model(x).reshape(batch_size, s, s)
+        out = y_normalizer.decode(out)
+        y = y_normalizer.decode(y)
+        
+        loss = myloss(out.view(batch_size,-1), y.view(batch_size,-1))
+        loss.backward()
+        optimizer.step()
+        train_l2 += loss.item()
+    
+    scheduler.step()
+    model.eval()
+    test_l2 = 0.0
+    with torch.no_grad():
+        for x, y in test_loader:
+            x, y = x.to(device), y.to(device)
+
+            out = model(x).reshape(batch_size, s, s)
+            out = y_normalizer.decode(out)
+
+            test_l2 += myloss(out.view(batch_size,-1), y.view(batch_size,-1)).item()
+
+    train_l2/= ntrain
+    test_l2 /= ntest
+    
+    train_loss[ep] = train_l2
+    test_loss[ep] = test_l2
+    
+    t2 = default_timer()
+    print(ep, t2-t1, train_l2, test_l2)
